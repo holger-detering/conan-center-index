18166b62f2b7168b4dd09feedbcd94d3d08d77d5
holger-detering <github@detering-springhoe.de>
2023-07-30 20:05:35 +0200

    aws-c-auth: add version 0.7.2

--- recipes/aws-c-auth/all/conanfile.py
+++ recipes/aws-c-auth/all/conanfile.py
@@ -44,15 +44,24 @@ class AwsCAuth(ConanFile):
         cmake_layout(self, src_folder="src")
 
     def requirements(self):
-        self.requires("aws-c-common/0.8.2", transitive_headers=True, transitive_libs=True)
-        self.requires("aws-c-cal/0.5.13")
+        if Version(self.version) < "0.7.2":
+            self.requires("aws-c-common/0.8.2", transitive_headers=True, transitive_libs=True)
+            self.requires("aws-c-cal/0.5.13")
+        else:
+            self.requires("aws-c-common/0.9.0", transitive_headers=True, transitive_libs=True)
+            self.requires("aws-c-cal/0.6.1")
         if Version(self.version) < "0.6.17":
             self.requires("aws-c-io/0.10.20", transitive_headers=True)
             self.requires("aws-c-http/0.6.13", transitive_headers=True)
-        else:
+        elif Version(self.version) < "0.7.2":
             self.requires("aws-c-io/0.13.4", transitive_headers=True)
             self.requires("aws-c-http/0.6.22", transitive_headers=True)
-        if Version(self.version) >= "0.6.5":
+        else:
+            self.requires("aws-c-io/0.13.30", transitive_headers=True)
+            self.requires("aws-c-http/0.7.11", transitive_headers=True)
+        if Version(self.version) >= "0.7.2":
+            self.requires("aws-c-sdkutils/0.1.12", transitive_headers=True)
+        elif Version(self.version) >= "0.6.5":
             self.requires("aws-c-sdkutils/0.1.3", transitive_headers=True)
 
     def source(self):
